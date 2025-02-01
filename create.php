6990559aaca23d6ff3aabf98cf66f022ababe186
yinhao <yinhaocx@gmail.com>
2025-01-31 22:11:53 -0500

    Update create.php

--- create.php
+++ create.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Link - ChatCYH URL Code</title>
+    <title>Create Link - URL Code</title>
     <style>
         body {
             margin: 0;
